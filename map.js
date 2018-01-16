df9075b87aede7f3715d06faaf603814d7bbe9b0
Abishek Aditya <abishekaditya@outlook.com>
2018-01-16 10:22:40 +0530

    Verifiable format for map

--- map.js
+++ map.js
@@ -2,14 +2,14 @@ function doubleAll(numbers) {
  return numbers.map( x => x * 2)  
 }
 
-//module.exports = doubleAll
-//
+module.exports = doubleAll
 
-function equals(firstArray, secondArray){
-  for(let i = 0; i < firstArray.length; i++){
-    if( firstArray[i] !== secondArray[i]) return false
-  }
-  return true
-}
-console.log("double empty array : ", equals(doubleAll([]),[]))
-console.log("double non-empty array : ", equals(doubleAll([1,2,3]),[2,4,6]))
+
+//function equals(firstArray, secondArray){
+//  for(let i = 0; i < firstArray.length; i++){
+//    if( firstArray[i] !== secondArray[i]) return false
+//  }
+//  return true
+//}
+//console.log("double empty array : ", equals(doubleAll([]),[]))
+//console.log("double non-empty array : ", equals(doubleAll([1,2,3]),[2,4,6]))
