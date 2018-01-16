3f2cdcdea4eb17dfe70982f318ecb3f150f5e2e4
Abishek Aditya <abishekaditya@outlook.com>
2018-01-16 14:20:19 +0530

    test cases passed for reduce

--- every-some.js
+++ every-some.js
@@ -4,23 +4,23 @@ function checkUsersValid(goodUsers) {
   };
 }
 
-//module.exports = checkUsersValid
+module.exports = checkUsersValid
 //
 
-console.log("empty arrays : ", (checkUsersValid([]))([]) === true)
-let good = [
-  { id: 1 },
-  { id: 2 },
-  { id: 3 }
-]
-let valid = [
-  { id: 1},
-  { id: 4}
-]
+//console.log("empty arrays : ", (checkUsersValid([]))([]) === true)
+//let good = [
+//  { id: 1 },
+//  { id: 2 },
+//  { id: 3 }
+//]
+//let valid = [
+//  { id: 1},
+//  { id: 4}
+//]
+//
+//console.log("some in valid are not in good : ", (checkUsersValid(good))(valid) === false)
+//let valid2 = [
+//  { id: 1}
+//]
 
-console.log("some in valid are not in good : ", (checkUsersValid(good))(valid) === false)
-let valid2 = [
-  { id: 1}
-]
-
-console.log("all in valid are in good : ", (checkUsersValid(good))(valid2) === true)
+//console.log("all in valid are in good : ", (checkUsersValid(good))(valid2) === true)
---
+++ reduce.js
@@ -0,0 +1,14 @@
+function countWords(inputWords) {
+  return inputWords.reduce(function (acc, val) {
+    if (val in acc) {
+      acc[val]++
+    }else {
+      acc[val] = 1
+    }
+    return acc
+  } , {})
+}
+
+//module.exports = countWords
+
+console.log("array with 2 cakes and 1 bake", countWords(["Cake", "Cake", "Bake"]).Cake === 2)
