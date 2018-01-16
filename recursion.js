e1a4635fe3a10b375858992d94e308dae883f4c9
Abishek Aditya <abishekaditya@outlook.com>
2018-01-16 14:26:06 +0530

    test cases passed for recursion

---
+++ recursion.js
@@ -0,0 +1,11 @@
+function reduce(arr, fn, initial) {
+  return (function reducer(index, previous){
+    if (arr.length <= index) return previous
+    return reducer(index + 1, fn(previous, arr[index], index, arr))
+  }(0, initial))
+}
+
+//module.exports = reduce
+//
+
+console.log("use reduce for summation", reduce([1,2,3], function(i,a) { return i + a } , 0) === 6)
