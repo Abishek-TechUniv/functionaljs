78126c2ed480d6604ca1726aaa0562c6d4256933
Abishek Aditya <abishekaditya@outlook.com>
2018-01-17 13:18:01 +0530

    refactored recursion

--- recursion.js
+++ recursion.js
@@ -12,5 +12,5 @@ module.exports = reduce
 
 //console.log("use reduce for summation", reduce([1,2,3], function(i,a) { return i + a } , 0) === 6)
 //console.log("use reduce with empty array", reduce([], function(i,a) { return i + a}, 0) === 0)
-//console.log("use reduce using index", reduce([1,2,3], function(a, i, ind){ return ((ind % 2 == 0) ? a + i : a )}, 0) == 4)
-//console.log("use reduce using array", reduce([1,2,3], function(a, i, ind, arr){ return ((arr.length - ind >= 0) ? a + i : a )}, 0) == 4)
+//console.log("use reduce using index", reduce([1,2,3], function(a, i, ind){ return ((ind % 2 == 0) ? a + i : a )}, 0) === 4)
+//console.log("use reduce using array", reduce([1,2,3], function(a, i, ind, arr){ return ((arr.length - ind >= 2) ? a + i : a )}, 0) === 3)
