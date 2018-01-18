f7d435e09471d5d05e79ae8fb449c2004c7373e2
Abishek Aditya <abishekaditya@outlook.com>
2018-01-18 10:26:03 +0530

    added test case for count

--- count.js
+++ count.js
@@ -8,12 +8,13 @@ function duckCount() {
 
 }
 
-module.exports = duckCount //eslint-disable-linenod
+module.exports = duckCount //eslint-disable-line
 
 console.log("passing nothing", duckCount() === 0) //eslint-disable-line
 console.log("passing 1 argument as quack", duckCount({quack: true}) === 1) //eslint-disable-line
 console.log("passing 1 argument duck using Object.create", duckCount(Object.create({quack: true})) === 0) //eslint-disable-line
 console.log("passing 2 args with none of them quack", duckCount(Object.create({duck: true}), {duck: 1}) === 0) //eslint-disable-line
 console.log("passing 2 args with 1 of them quack", duckCount(Object.create({duck: true}), {quack: 1}) === 1) //eslint-disable-line
+console.log("passing 2 args with 1 of them quack and 1 not", duckCount(Object.create({Quack: true}), {quack: 1}) === 1) //eslint-disable-line
 console.log("passing 2 args with all of them quack", duckCount({quack: 2}, {quack: 1}) === 2) //eslint-disable-line
 console.log("passing 2 args with all of them quack, one using Object.create", duckCount(Object.create({quack: true}), {quack: 1}) === 1) //eslint-disable-line
